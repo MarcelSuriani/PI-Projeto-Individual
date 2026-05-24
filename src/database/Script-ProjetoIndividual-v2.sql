f3a8f499e1a4c5ce018a1921f17031454c00f638
MarcelSuriani <marcel.ferreira@sptech.school>
2026-05-24 01:29:25 -0300

    Feat: Ajustes no script do banco de dados - Adição de novas tabelas

--- src/database/Script-ProjetoIndividual-v2.sql
+++ src/database/Script-ProjetoIndividual-v2.sql
@@ -21,8 +21,8 @@ respostaCorreta CHAR(20)
 CREATE TABLE tentativa (
 idTentativa INT PRIMARY KEY AUTO_INCREMENT,
 fkUsuario INT,
-dataResposta DATETIME DEFAULT CURRENT_TIMESTAMP,
 totalAcertos INT,
+dataResposta DATETIME DEFAULT CURRENT_TIMESTAMP,
 CONSTRAINT teqfkUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
 );
 
@@ -45,6 +45,15 @@ fkHabilidade INT,
 CONSTRAINT favfkUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
 CONSTRAINT favfkSkin FOREIGN KEY (fkSkin) REFERENCES skin(idSkin),
 CONSTRAINT favfkHabilidade FOREIGN KEY (fkHabilidade) REFERENCES habilidade(idHabilidade)
+);
+
+CREATE TABLE tentativaQuestao (
+idTentativaQuestao INT PRIMARY KEY AUTO_INCREMENT,
+fkTentativa INT,
+fkQuestao INT,
+acertou BOOLEAN,
+CONSTRAINT tqfkTentativa FOREIGN KEY (fkTentativa) REFERENCES tentativa(idTentativa),
+CONSTRAINT tqfkQuestao FOREIGN KEY (fkQuestao) REFERENCES questao(idQuestao)
 );
 
 INSERT INTO habilidade VALUES
@@ -74,9 +83,25 @@ INSERT INTO skin VALUES
 (DEFAULT,'Shaco Pandemônio de Prestígio'),
 (DEFAULT,'Shaco McDonalds');
 
+INSERT INTO questao (pergunta, respostaCorreta) VALUES
+('O que Shaco era antes de ser corrompido pelo caos?', 'alternativaB'),
+('Shaco mata por ódio e busca vingança contra quem destruiu sua vida.', 'alternativaB'),
+('Qual é o efeito da passiva do Shaco, Apunhalar?', 'alternativaB'),
+('O que acontece quando o clone do Shaco (R) morre?', 'alternativaC'),
+('O Q do Shaco causa dano crítico ao atacar pelas costas enquanto invisível.', 'alternativaA'),
+('Qual habilidade do Shaco causa medo nos inimigos?', 'alternativaB'),
+('Qual skin do Shaco é inspirada em Alice no País das Maravilhas?', 'alternativaC'),
+('O caos que habita Shaco criou uma consciência humana com emoções e empatia.', 'alternativaB'),
+('Segundo a lore, como Shaco foi criado?', 'alternativaB'),
+('Qual habilidade envenena os inimigos passivamente e causa dano extra abaixo de 30% de vida?', 'alternativaC'),
+('Qual detalhe oculto conecta Shaco diretamente ao conceito de caos?', 'alternativaC'),
+('O que é o que é? Um palhaço sorridente, vestido de amarelo e vermelho.', 'alternativaD');
+
 SELECT * FROM usuario;
 SELECT * FROM questao;
 SELECT * FROM tentativa;
 SELECT * FROM skin;
 SELECT * FROM habilidade;
 SELECT * FROM favorito;
+
+SELECT * FROM tentativaQuestao;
